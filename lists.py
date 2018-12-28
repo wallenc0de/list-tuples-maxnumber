bf4dd45180975570227907000266edc2673eeb7a
wallenc0de <wallencode@protonmail.com>
2018-12-28 20:49:34 +0100

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,4 +1,7 @@
-# This only contains examples with a list of lists and tuples.
+# Tuples and lists are basically the same thing they are both containers that store different pieces of informations, however there's a few key differences that makes tuples unique to lists.
+# Just as strings and numbers they are immutable, once you create a tuple it does it as it is.
+
+# I personally would use tuples in data where i would not change or modify them.
 
 def lists_tuples():
     print('I loaded \'people\' and \'tuples\' list.\n')
@@ -20,8 +23,8 @@ def lists_tuples():
         print(f"\n{y[0]} - {y[1]} - {y[2]}")
 
 def say_hi(name):
-    print("Hi " + name +".") # im often not using f-string, because the difference in speed is really minimal.
+    print("Hi " + name +".") # I'm often not using f-string, because the difference in speed is really minimal.
 
-# The string inside the function call is only a piece of information that we are setting to "name" parameter.
-say_hi("wallen")
+
+say_hi("wallen") # The string inside the function call is only a piece of information that we are setting to "name" parameter.
 lists_tuples()
