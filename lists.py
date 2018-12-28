7c6b3ff49f7c3c1a1f817b47b6f9efb130741fc7
wallenc0de <wallencode@protonmail.com>
2018-12-28 16:56:02 +0100

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,1 +1,15 @@
+# Testing repository, this file contains examples with tuples and a list of lists.
 
+def lists():
+    print('I loaded \'people\' list.\n')
+    people = [
+        ["User_1", 20],
+        ["User_2", 30],
+        ["User_3", 14],
+    ]
+
+def say_hi(name):
+    print(f"Hi {name}.")
+
+say_hi("wallen")
+lists()
